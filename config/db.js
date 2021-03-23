98e317ed8347a26bad97cc7a48958d1202ba3feb
Emmytobs <real4emmydee@gmail.com>
2021-03-23 13:41:20 +0100

    Added heroku config for production environment

--- config/db.js
+++ config/db.js
@@ -1,13 +1,21 @@
 const { Pool } = require('pg');
 
 // PostgreSQL config goes here
-const pool = new Pool({
+
+const developmentConfig = {
     user: process.env.PG_USER,
     password: process.env.PG_PASSWORD,
     port: process.env.PG_PORT,
     host: process.env.PG_HOST,
     database: process.env.PG_DATABASE,
-})
+}
+const productionConfig = {
+    connectionString: process.env.DATABASE_URL
+}
+
+const pool = new Pool(
+    process.env.NODE_ENV === 'production' ? productionConfig : developmentConfig
+);
 
 pool.connect()
     .then(() => console.log('Postgres Database connected successfully'))
